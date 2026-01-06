ac895ecb0762249f8e448a389ea056e3feb4bfd4
Yober Cieza <65102016+yoberxito@users.noreply.github.com>
2026-01-05 20:18:47 -0500

    Update api.py

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ def load_model():
     global model
     model = joblib.load("modelo_sentimientos.pkl")
 
-@app.post("/sentiment")
+@app.route("/sentiment", methods=["POST"])
 def predict_sentiment(request: SentimentRequest):
 
     prediction = model.predict([request.text])[0]
@@ -24,3 +24,4 @@ def predict_sentiment(request: SentimentRequest):
         "prevision": "Positivo" if prediction == 1 else "Negativo",
         "probabilidad": float(probability)
     }
+
